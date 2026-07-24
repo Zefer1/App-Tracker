254a83962cc5fa4ee636e7fe4733f8f8f5a0a37f
José Fernandes <ze_932@hotmail.com>
2026-07-24 17:14:44 +0100

    set up react routes with react router

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <img
+      src="https://static.wikia.nocookie.net/theloudhousefanon/images/9/96/John_Xina.webp/revision/latest?cb=20230820051413"
+      alt="Katherine Johnson"
+    />
+  )
+}
