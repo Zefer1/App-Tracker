20693768a2446417e505674260c8cb7526c77757
José Fernandes <ze_932@hotmail.com>
2026-07-26 18:23:42 +0100

    list applications for authenticated user

--- client/src/pages/Applications.tsx
+++ client/src/pages/Applications.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react"
+import type { Application } from "../types/application";
+
 export default function Applications() {
+  const [applications, setApplications] = useState<Application[]>([]);
+  const [erro, setErro] = useState('');
+
+  useEffect(() => {
+    async function fecthApplications() {
+      try {
+        const response = await fetch('http://localhost:3000/api/applications', {credentials: 'include'}); 
+        if(response.ok){
+        const data = await response.json()
+        setApplications(data)
+        } else {
+          const data = await response.json()
+          setErro(data.error)
+        }
+      } catch (error) {
+        console.log(error)
+        setErro('Não foi possível ligar ao servidor. Verifica a tua ligação.');
+      }
+    }
+    fecthApplications();
+  },[])
+
+  const listApplications = applications.map(application => 
+    <li key={application.id}>
+      <h2>{application.company}</h2>
+      <p>{application.status}</p>
+      <p>{application.position}</p>
+    </li>
+  );
+
   return (
-    <img
-      src="https://static.wikia.nocookie.net/theloudhousefanon/images/9/96/John_Xina.webp/revision/latest?cb=20230820051413"
-      alt="Katherine Johnson"
-    />
+    
+    <div>
+      <p>{erro}</p>
+      <ul>{listApplications}</ul>
+    </div>
   )
 }
