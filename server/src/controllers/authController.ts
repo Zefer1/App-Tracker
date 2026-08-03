717c3ffd740a56fc1a3092a6f68348103ef6fdff
José Fernandes <ze_932@hotmail.com>
2026-08-03 17:56:05 +0100

    samesite res.cookie change to fix deploy

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -36,7 +36,7 @@ export async function login (req: Request, res: Response) {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 8 * 60 * 60 * 1000
     });
 
@@ -49,7 +49,7 @@ export async function logout (req: Request, res: Response) {
     res.clearCookie('token', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     });
 
     res.status(200).json({ message: 'Logout efetuado com sucesso' });
